eea2f592cda71e303ccd22a8080470fb3d2ed4b2
Justin Luke <justin.luke@berkeley.edu>
2016-02-12 01:07:56 -0800

    Completed HW

--- foobar.rb
+++ foobar.rb
@@ -1,5 +1,12 @@
 class Foobar
   # Q4 CODE HERE
-  def baz
-	
+	def self.baz(a)
+		a.map{|i| (i.to_i+2)}.select{|i| i<=10 && i.even?}.uniq.inject(0){|sum,x| sum + x}
+	end
 end
+
+
+'''Write a class function in foobar.rb called baz that takes in an array of integers as strings,
+changes each string into an integer, adds two to each number, keeps the even numbers only, takes out
+any duplicates, rejects the resulting numbers greater than ten, and returns the sum of the resulting
+array.'''
